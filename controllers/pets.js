d9c2a27cea0e955436ba1998a6e5a07a18adc762
Abdulla Dashti <abdullamustafadashti.0@gmail.com>
2025-10-25 14:50:23 +0300

    index route

--- controllers/pets.js
+++ controllers/pets.js
@@ -2,6 +2,16 @@ const Pet = require('../models/pet.js');
 const express = require('express');
 const router = express.Router();
 
+router.get('/', async (req, res) => {
+  try {
+    const foundPets = await Pet.find();
+    res.status(200).json(foundPets);
+  } catch (err) {
+    res.status(500).json({ err: err.message }); 
+  }
+});
+
+
 router.post('/', async (req, res) => {
   try {
     const createdPet = await Pet.create(req.body);
